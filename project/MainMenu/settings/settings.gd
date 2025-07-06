52024b26a9dc0755194df261f890096ef90220f5
Canapca <canapca-dev@proton.me>
2025-07-06 13:59:31 +0100

    Fixed your code, dumbass

--- project/MainMenu/settings/settings.gd
+++ project/MainMenu/settings/settings.gd
@@ -1,10 +1,7 @@
 extends Control
 
-func _on_button_button_down() -> void:
-	# active
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-
-
-func _on_button_button_up() -> void:
-	# inactive
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+func _on_button_toggled(toggled_on: bool) -> void:
+	if !toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
