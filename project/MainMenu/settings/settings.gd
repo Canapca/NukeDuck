4bb960db7d37a4859ba682b1e60be5a57969e2ef
Canapca <canapca-dev@proton.me>
2025-07-09 21:45:23 +0100

    Fixed window tweaking out when changing between fullscreen and windowed

--- project/MainMenu/settings/settings.gd
+++ project/MainMenu/settings/settings.gd
@@ -5,3 +5,4 @@ func _on_button_toggled(toggled_on: bool) -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		DisplayServer.window_set_size(Vector2(1920/2, 1080/2))
