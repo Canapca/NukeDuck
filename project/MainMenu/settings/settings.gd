ccdc0d1a8de50754e968f282a6bd6b7af66cca53
WhosAspiire <gpluzyczka@gmail.com>
2025-07-09 21:33:12 +0100

    Ha! Windows 🍵

--- project/MainMenu/settings/settings.gd
+++ project/MainMenu/settings/settings.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_button_toggled(toggled_on: bool) -> void:
-	if !toggled_on:
+	if ! toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
